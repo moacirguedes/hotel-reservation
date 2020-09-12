69e6ecbc2517e774d3582c2aa5b8e3104cb62898
Moacir <moacirgo97@gmail.com>
2020-09-12 13:18:03 -0300

    seed for manager

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +22,14 @@ class DatabaseSeeder extends Seeder
             RoomSeeder::class,
             ReservationSeeder::class,
             ReservationsRoomsSeeder::class
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Manager',
+            'email' => 'user@manager.com',
+            'password' => Hash::make('password'),
+            'isManager' => true,
+            'remember_token' => Str::random(10),
         ]);
     }
 }
